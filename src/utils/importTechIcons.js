5a8eae25de5a1b9a6c4eb806d2e955b3ea8725ee
Carlos Fernandez <dominguezalacid@gmail.com>
2024-06-01 21:06:13 +0200

    actualizar

--- src/utils/importTechIcons.js
+++ src/utils/importTechIcons.js
@@ -1,9 +1,33 @@
-// utils/importTechIcons.js
+// src/utils/importTechIcons.js
+import angular from '../assets/icons/angular-icon.png';
+import css from '../assets/icons/css-icon.png';
+import docker from '../assets/icons/docker-icon.png';
+import git from '../assets/icons/git-icon.png';
+import github from '../assets/icons/github-icon.png';
+import html from '../assets/icons/html-icon.png';
+import ionic from '../assets/icons/ionic-icon.png';
+import java from '../assets/icons/java-icon.png';
+import javascript from '../assets/icons/javascrit-icon.png';
+import maven from '../assets/icons/maven-icon.png';
+import mongo from '../assets/icons/mongo-icon.png';
+import photoshop from '../assets/icons/photoshop-icon.png';
 
-export function importTechIcons(r) {
-    let images = {};
-    r.keys().forEach((item) => {
-        images[item.replace("../assets/icons/", "")] = r(item);
-    });
-    return images;
-}
+
+
+const techIcons = {
+    'angular-icon.png': angular,
+    'css-icon.png': css,
+    'docker-icon.png': docker,
+    'git-icon.png': git,
+    'github-icon.png': github,
+    'html-icon.png': html,
+    'ionic-icon.png': ionic,
+    'java-icon.png': java,
+    'javascrit-icon.png': javascript,
+    'maven-icon.png': maven,
+    'mongo-icon.png': mongo,
+    'photoshop-icon.png': photoshop
+    // Añade todos tus íconos aquí
+};
+
+export default techIcons;
